4eb7822eee7f0e2ae2719a3c7883732d70567c57
Thomas Gelf <thomas@gelf.net>
2019-05-15 09:49:54 +0200

    EventDetailsTable: disable step by step...
    ...it's pretty obsolete right now

--- library/Eventtracker/Web/Table/EventDetailsTable.php
+++ library/Eventtracker/Web/Table/EventDetailsTable.php
@@ -12,20 +12,46 @@ class EventDetailsTable extends NameValueTable
 {
     use TranslationHelper;
 
+    // HINT: This renders nothing right now.
+
     public function __construct(Incident $incident)
     {
         $this->addNameValuePairs([
-            $this->translate('Since')    => Time::agoFormatted($incident->get('ts_first_event')),
-            $this->translate('Status')   => $incident->get('status'),
-            $this->translate('Severity') => $incident->get('severity'),
-            $this->translate('Priority') => $incident->get('priority'),
-            $this->translate('Host')     => $incident->get('host_name'),
-            $this->translate('Object')   => $incident->get('object_name'),
-            $this->translate('Class')    => $incident->get('object_class'),
-            $this->translate('Message')  => HtmlPurifier::process($incident->get('message')),
-            $this->translate('Owner')    => $incident->get('owner', '-'),
+            // $this->translate('Since')    => Time::agoFormatted($incident->get('ts_first_event')),
+            // $this->translate('Status')   => $incident->get('status'),
+            // $this->translate('Severity') => $incident->get('severity'),
+            // $this->translate('Priority') => $incident->get('priority'),
+            // $this->translate('Host')     => $incident->get('host_name'),
+            // $this->translate('Object')   => $incident->get('object_name'),
+            // $this->translate('Class')    => $incident->get('object_class'),
+            // $this->translate('Message')  => HtmlPurifier::process($incident->get('message')),
+            // this->translate('Owner')    => $incident->get('owner', '-'),
         ]);
 
-        $this->addNameValuePairs($incident->getProperties());
+        $blacklist = [
+            'severity',
+            'priority',
+            'status',
+            'message',
+            'host_name',
+            'owner',
+            'cnt_events',
+            'object_name',
+            'object_class',
+            'incident_uuid',
+            'ts_first_event',
+            'ts_last_modified',
+            'ts_expiration',
+
+            'sender_id',
+            'sender_event_id',
+            'sender_event_checksum',
+        ];
+        foreach ($incident->getProperties() as $name => $value) {
+            if (in_array($name, $blacklist)) {
+                continue;
+            }
+            $this->addNameValueRow($name, $value);
+        }
     }
 }
