fe05cdf9a9ce5a8d8b8e7ee2e2b82800696f069b
Thomas Gelf <thomas@gelf.net>
2019-05-15 10:10:19 +0200

    LinkLikeForm: shortcut for link-like form buttons

---
+++ library/Eventtracker/Web/Form/LinkLikeForm.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Icinga\Module\Eventtracker\Web\Form;
+
+use gipfl\IcingaWeb2\Icon;
+use Icinga\Module\Eventtracker\Web\Form;
+use ipl\Html\Html;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class LinkLikeForm extends Form
+{
+    /** @var string */
+    protected $linkLabel;
+
+    /** @var string|null */
+    protected $linkTitle;
+
+    /** @var string|null */
+    protected $linkIcon;
+
+    /**
+     * TestForm constructor.
+     * @param string|$linkLabel
+     * @param string|null $linkIcon
+     */
+    public function __construct($linkLabel, $linkTitle = null, $linkIcon = null)
+    {
+        $this->linkLabel = (string) $linkLabel;
+        if ($linkTitle !== null) {
+            $this->linkTitle = (string) $linkTitle;
+        }
+        if ($linkIcon !== null) {
+            $this->linkIcon = (string) $linkIcon;
+        }
+        $this->setMethod('POST');
+        $this->addAttributes(['class' => 'inline']);
+        $this->styleWithDirector();
+    }
+
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response = null)
+    {
+        $this->handleRequest($request);
+    }
+
+    protected function assemble()
+    {
+        if ($this->linkIcon) {
+            $this->add(Icon::create($this->linkIcon, [
+                'class' => 'link-color'
+            ]));
+        }
+        $this->addElement('submit', 'submit', [
+            'class' => 'link-button',
+            'title' => $this->linkTitle,
+            'label' => $this->linkLabel
+        ]);
+    }
+}
