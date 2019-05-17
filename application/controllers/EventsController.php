72c4775ac47eaefff814d0287f9f52942003bfe8
Thomas Gelf <thomas@gelf.net>
2019-05-17 11:42:10 +0200

    EventsController: default sort. Still manual.

--- application/controllers/EventsController.php
+++ application/controllers/EventsController.php
@@ -5,6 +5,7 @@ namespace Icinga\Module\Eventtracker\Controllers;
 use gipfl\IcingaWeb2\CompatController;
 use Icinga\Authentication\Auth;
 use Icinga\Module\Eventtracker\DbFactory;
+use Icinga\Module\Eventtracker\Uuid;
 use Icinga\Module\Eventtracker\Web\Table\EventsTable;
 use Icinga\Module\Eventtracker\Web\Widget\AdditionalTableActions;
 
@@ -12,9 +13,13 @@ class EventsController extends CompatController
 {
     public function indexAction()
     {
+        $this->setAutorefreshInterval(5);
         $db = DbFactory::db();
 
         $table = new EventsTable($db);
+        if (! $this->url()->getParam('sort')) {
+            $this->url()->setParam('sort', 'severity DESC');
+        }
         if ($this->params->get('view') === 'compact') {
             $table->setNoHeader();
             $table->search($this->params->get('q'));
