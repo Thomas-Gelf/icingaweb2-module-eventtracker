884659847958bdea895ce9f9b8e7d7c92d851a50
Thomas Gelf <thomas@gelf.net>
2019-06-14 10:05:01 +0200

    EventsController: fit recent changes

--- application/controllers/EventsController.php
+++ application/controllers/EventsController.php
@@ -3,23 +3,88 @@
 namespace Icinga\Module\Eventtracker\Controllers;
 
 use gipfl\IcingaWeb2\CompatController;
+use gipfl\IcingaWeb2\Link;
 use Icinga\Authentication\Auth;
+use Icinga\Module\Eventtracker\Db\EventSummaryByPriority;
+use Icinga\Module\Eventtracker\Db\EventSummaryBySeverity;
 use Icinga\Module\Eventtracker\DbFactory;
 use Icinga\Module\Eventtracker\Uuid;
 use Icinga\Module\Eventtracker\Web\Table\EventsTable;
 use Icinga\Module\Eventtracker\Web\Widget\AdditionalTableActions;
+use Icinga\Module\Eventtracker\Web\Widget\SummaryFilter;
+use Icinga\Web\Widget\Tabextension\DashboardAction;
+use ipl\Html\Html;
 
 class EventsController extends CompatController
 {
+    protected function showCompact()
+    {
+        return $this->params->get('view') === 'compact';
+    }
+
+    protected function applyFilters(EventsTable $table)
+    {
+        $table->search($this->params->get('q'));
+        $this->columnFilter($table, 'host_name', 'hosts', $this->translate('Hosts: %s'));
+        $this->columnFilter($table, 'object_class', 'classes', $this->translate('Classes: %s'));
+        $this->columnFilter($table, 'object_name', 'objects', $this->translate('Objects: %s'));
+        $this->columnFilter($table, 'owner', 'owners', $this->translate('Owners: %s'));
+    }
+
+    protected function columnFilter($table, $column, $controller, $title)
+    {
+        $compact = $this->showCompact();
+        if ($this->params->has($column)) {
+            $value = $this->params->get($column);
+            if (strlen($value)) {
+                $table->getQuery()->where("$column = ?", $value);
+            } else {
+                $table->getQuery()->where("$column IS NULL");
+                $value = $this->translate('- none -');
+            }
+            if ($compact) {
+                return;
+            }
+            $this->content()->add(
+                Link::create(
+                    sprintf($title, $value),
+                    $this->url()->without($column),
+                    null,
+                    ['data-base-target' => '_self']
+                )
+            );
+        } else {
+            if ($compact) {
+                return;
+            }
+            $this->content()->add(
+                Link::create(
+                    sprintf($title, $this->translate('all')),
+                    "eventtracker/$controller",
+                    null,
+                    ['data-base-target' => '_next']
+                )
+            );
+        }
+        $this->content()->add(Html::tag('br'));
+    }
+
     public function indexAction()
     {
         $this->setAutorefreshInterval(5);
         $db = DbFactory::db();
 
         $table = new EventsTable($db);
+        $this->applyFilters($table);
+
+        $prioSummary = new EventSummaryByPriority($table->getQuery());
+        $sevSummary = new EventSummaryBySeverity($table->getQuery());
+        $sevSummary->filterByUrl($this->url());
+        if (! $this->showCompact()) {
+            $this->actions()->prepend(new SummaryFilter($sevSummary->fetch($db), $this->url()));
+        }
 
         if ($this->getRequest()->isApiRequest()) {
-            $table->search($this->params->get('q'));
             $table->handleSortUrl($this->url());
             $result = $table->fetch();
             foreach ($result as & $row) {
@@ -32,11 +97,11 @@ class EventsController extends CompatController
         if (! $this->url()->getParam('sort')) {
             $this->url()->setParam('sort', 'severity DESC');
         }
-        if ($this->params->get('view') === 'compact') {
+        if ($this->showCompact()) {
             $table->setNoHeader();
-            $table->search($this->params->get('q'));
-            $table->handleSortUrl($this->url());
             $this->content()->add($table);
+            $table->handleSortUrl($this->url());
+            $table->getQuery()->limit(10);
         } else {
             $this->addSingleTab('Events');
             $this->addTitle('Event Tracker');
@@ -44,6 +109,10 @@ class EventsController extends CompatController
                 ->appendTo($this->actions());
             $table->handleSortUrl($this->url());
             $table->renderTo($this);
+        }
+
+        if (! $this->showCompact()) {
+            $this->tabs()->extend(new DashboardAction());
         }
     }
 }
