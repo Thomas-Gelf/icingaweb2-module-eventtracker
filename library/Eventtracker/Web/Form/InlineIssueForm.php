511e9e89c225df591734f6fa11f53621b2bbb45f
Thomas Gelf <thomas@gelf.net>
2019-09-04 10:44:23 +0200

    InlineIssueForm: allow 2 override hasBeenSubmitted

--- library/Eventtracker/Web/Form/InlineIssueForm.php
+++ library/Eventtracker/Web/Form/InlineIssueForm.php
@@ -15,6 +15,9 @@ abstract class InlineIssueForm extends Form
     /** @var Issue */
     protected $issue;
 
+    /** @var boolean|null */
+    protected $hasBeenSubmitted;
+
     public function __construct(Issue $issue, DbAdapter $db)
     {
         $this->issue = $issue;
@@ -43,6 +46,22 @@ abstract class InlineIssueForm extends Form
         $this->toggleNextSubmitCancel($next, $submit, $cancel);
     }
 
+    public function setSubmitted($submitted = true)
+    {
+        $this->hasBeenSubmitted = (bool) $submitted;
+
+        return $this;
+    }
+
+    public function hasBeenSubmitted()
+    {
+        if ($this->hasBeenSubmitted === null) {
+            return parent::hasBeenSubmitted();
+        } else {
+            return $this->hasBeenSubmitted;
+        }
+    }
+
     protected function toggleNextSubmitCancel(
         SubmitElement $next,
         SubmitElement $submit,
@@ -52,8 +71,13 @@ abstract class InlineIssueForm extends Form
             $this->addElement($submit);
             $this->addElement($cancel);
             if ($cancel->hasBeenPressed()) {
+                // HINT: we might also want to redirect on cancel and stop here,
+                //       but currently we have no Response
+                $this->setSubmitted(false);
                 $this->remove($submit);
                 $this->remove($cancel);
+                $this->add($next);
+                $this->setSubmitButton($next);
             } else {
                 $this->setSubmitButton($submit);
                 $this->remove($next);
