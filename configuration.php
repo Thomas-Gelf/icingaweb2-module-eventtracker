a1a0f74bbe126ec5358b16600460491f5567d8c1
Thomas Gelf <thomas@gelf.net>
2019-09-08 23:16:35 +0200

    configuration: show available permissions

--- configuration.php
+++ configuration.php
@@ -9,6 +9,14 @@ $section->add(N_('Issues'))->setUrl('eventtracker/issues');
 $section->add(N_('Summaries'))->setUrl('eventtracker/summary/top10');
 
 $this->providePermission(
+    'eventtracker/admin',
+    $this->translate('Eventtracker admin')
+);
+$this->providePermission(
+    'eventtracker/showsql',
+    $this->translate('Allow to show SQL queries')
+);
+$this->providePermission(
     'eventtracker/operator',
     $this->translate('Operators are allowed to modify issues (Priority, Owner...)')
 );
