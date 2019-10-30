0c3d09f2de73f067dad3a8796dfc62ce90a8793f
Thomas Gelf <thomas@gelf.net>
2019-10-30 17:41:58 +0100

    configuration: change icon, prio

--- configuration.php
+++ configuration.php
@@ -2,9 +2,9 @@
 
 /** @var \Icinga\Application\Modules\Module $this */
 $section = $this->menuSection(N_('Event Tracker'))
-    ->setIcon('tasks')
+    ->setIcon('attention-circled')
     ->setUrl('eventtracker/dashboard')
-    ->setPriority(86);
+    ->setPriority(62);
 $section->add(N_('Issues'))->setUrl('eventtracker/issues');
 $section->add(N_('Summaries'))->setUrl('eventtracker/summary/top10');
 
