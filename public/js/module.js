310ceb0d821c3d30d20a33fffa426c4df3694508
Thomas Gelf <thomas@gelf.net>
2019-11-13 11:26:17 +0100

    js: introduce JS to avoid sort changes when...
    ...clicking the header row

---
+++ public/js/module.js
@@ -0,0 +1,22 @@
+(function(Icinga) {
+
+    var Eventtracker = function (module) {
+        this.module = module;
+        this.initialize();
+        this.module.icinga.logger.debug('EventTracker module loaded');
+    };
+
+    Eventtracker.prototype = {
+        initialize: function () {
+            this.module.on('mouseover', 'thead tr', this.checkForHeaderHref);
+        },
+
+        checkForHeaderHref: function (ev) {
+            // href will be added because of sort icons
+            $(ev.currentTarget).removeAttr('href');
+        }
+    };
+
+    Icinga.availableModules.eventtracker = Eventtracker;
+
+}(Icinga));
