549182ae0894fe6da0370667eb0399d5e9810779
Thomas Gelf <thomas@gelf.net>
2019-11-14 09:52:47 +0100

    ScomSync: close outdated issues

--- library/Eventtracker/Scom/ScomSync.php
+++ library/Eventtracker/Scom/ScomSync.php
@@ -2,7 +2,9 @@
 
 namespace Icinga\Module\Eventtracker\Scom;
 
+use Icinga\Module\Eventtracker\Event;
 use Icinga\Module\Eventtracker\EventReceiver;
+use Icinga\Module\Eventtracker\Issue;
 use Icinga\Module\Eventtracker\ObjectClassInventory;
 use Icinga\Module\Eventtracker\SenderInventory;
 use Zend_Db_Adapter_Abstract as DbAdapter;
@@ -11,6 +13,8 @@ use Zend_Db_Adapter_Pdo_Mssql as Mssql;
 class ScomSync
 {
     protected $db;
+
+    protected $senderId;
 
     public function __construct(DbAdapter $db)
     {
@@ -37,15 +41,46 @@ class ScomSync
     protected function shipEvents($events)
     {
         $db = $this->db;
-        $senders = new SenderInventory($db);
-        $sender = $senders->getSenderId('SCOM', 'new-scom');
         $classes = new ObjectClassInventory($db);
-        $factory = new ScomEventFactory($sender, $classes);
+        $senderId = $this->getSenderId();
+        $factory = new ScomEventFactory($senderId, $classes);
         $receiver = new EventReceiver($db);
 
+        $issuesFromScom = [];
         foreach ($events as $scomEvent) {
             $event = $factory->fromPlainObject($scomEvent);
             $issue = $receiver->processEvent($event);
+            if ($issue) {
+                $issuesFromScom[$event->get('sender_event_id')] = $issue->getUuid();
+            }
         }
+
+        foreach ($this->fetchExistingEventIds() as $eventId => $issueUuid) {
+            if (! isset($issuesFromScom[$eventId])) {
+                Issue::recoverUuid($issueUuid, $db);
+            }
+        }
+    }
+
+    protected function getSenderId()
+    {
+        if ($this->senderId === null) {
+            $senders = new SenderInventory($this->db);
+            $this->senderId = $senders->getSenderId('SCOM', 'new-scom');
+        }
+
+        return $this->senderId;
+    }
+
+    protected function fetchExistingEventIds()
+    {
+        $db = $this->db;
+        $ids = $db->fetchPairs(
+            $db->select()
+                ->from('issue', ['sender_event_id', 'issue_uuid'])
+                ->where('sender_id = ?', $this->getSenderId())
+        );
+
+        return $ids;
     }
 }
