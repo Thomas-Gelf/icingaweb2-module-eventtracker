3d5433abca9ebdef0bbaf2190aeeafca43240699
Thomas Gelf <thomas@gelf.net>
2020-02-07 13:34:45 +0100

    IcingaCi: add loading logic

--- library/Eventtracker/IcingaCi.php
+++ library/Eventtracker/IcingaCi.php
@@ -2,11 +2,14 @@
 
 namespace Icinga\Module\Eventtracker;
 
+use Icinga\Application\Config;
 use Zend_Db_Adapter_Abstract as Db;
 
 class IcingaCi
 {
     protected static $tableName = 'icinga_ci';
+
+    protected static $tableNameVars = 'icinga_ci_var';
 
     /**
      * @param Db $db
@@ -25,5 +28,84 @@ class IcingaCi
             $query->where('service_name = ?', $service);
         }
         return $db->fetchOne($query) > 0;
+    }
+
+    public static function eventuallyLoad(Db $db, $hostname, $service = null)
+    {
+        $object = static::eventuallyFetchCi($db, $hostname, $service);
+        if ($object) {
+            return $object;
+        } else {
+            $domain = \trim(Config::module('eventtracker')->get('ido-sync', 'search_domain'), '.');
+            if ($domain) {
+                return static::eventuallyFetchCi($db, "$hostname.$domain", $service);
+            }
+        }
+
+        return null;
+    }
+
+    protected static function eventuallyFetchCi(Db $db, $hostname, $service = null)
+    {
+        $query = self::prepareCiQuery($db)->where('host_name = ?', $hostname);
+
+        if ($service === null) {
+            $query->where('object_type = ?', 'host');
+        } else {
+            $query->where('service_name = ?', $service);
+        }
+
+        $ci = $db->fetchRow($query);
+        if ($ci) {
+            $ci->vars = static::fetchCiVars($db, $ci->object_id);
+            return $ci;
+        } else {
+            return null;
+        }
+    }
+
+    protected static function fetchCi(Db $db, $id)
+    {
+        $query = self::prepareCiQuery($db)->where('object_id = ?', $id);
+
+        $ci = $db->fetchRow($query);
+        if ($ci) {
+            $ci->vars = static::fetchCiVars($db, $ci->object_id);
+            return $ci;
+        } else {
+            return null;
+        }
+    }
+
+    protected static function prepareCiQuery(Db $db)
+    {
+        return $db->select()->from(self::$tableName, [
+            'object_id',
+            'host_id',
+            'object_type',
+            // 'checksum',
+            'host_name',
+            'service_name',
+            'display_name',
+        ]);
+    }
+
+    protected static function fetchCiVars(Db $db, $id)
+    {
+        $query = $db->select()->from(self::$tableNameVars, [
+            'varname',
+            'varvalue',
+            'varformat',
+        ]);
+        $vars = [];
+        foreach ($db->fetchAll($query) as $var) {
+            if ($var->varformat === 'json') {
+                $vars[$var->varname] = \json_decode($var->varvalue);
+            } else {
+                $vars[$var->varname] = (string) $var->varvalue;
+            }
+        }
+
+        return (object) $vars;
     }
 }
