35f26bbe640525b0056f57b299e8d56385857793
Thomas Gelf <thomas@gelf.net>
2020-03-27 11:02:33 +0100

    Controller: deal with BaseTables

--- application/controllers/Controller.php
+++ application/controllers/Controller.php
@@ -3,14 +3,31 @@
 namespace Icinga\Module\Eventtracker\Controllers;
 
 use gipfl\IcingaWeb2\CompatController;
+use Icinga\Authentication\Auth;
 use Icinga\Module\Eventtracker\Uuid;
 use Icinga\Module\Eventtracker\Web\Table\BaseTable;
+use Icinga\Module\Eventtracker\Web\Widget\AdditionalTableActions;
 
-class Controller extends CompatController
+abstract class Controller extends CompatController
 {
     protected function showCompact()
     {
         return $this->params->get('view') === 'compact';
+    }
+
+    protected function addTable(BaseTable $table, $defaultSort, $defaultLimit = 25)
+    {
+        if (! $this->url()->getParam('sort')) {
+            // This is required to trigger sort detection
+            $this->url()->setParam('sort', $defaultSort);
+        }
+
+        (new AdditionalTableActions($table, Auth::getInstance(), $this->url()))
+            ->appendTo($this->actions());
+
+        $table->getQuery()->limit($defaultLimit);
+        $this->eventuallySendJson($table);
+        $table->renderTo($this);
     }
 
     protected function eventuallySendJson(BaseTable $table)
