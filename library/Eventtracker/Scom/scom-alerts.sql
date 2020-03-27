f08fd69a1a50dae0fddf7ba0c05e2fb3d24a895b
Thomas Gelf <thomas@gelf.net>
2020-03-27 09:47:17 +0100

    scom-alerts: add maintenance, ignore severity=0

--- library/Eventtracker/Scom/scom-alerts.sql
+++ library/Eventtracker/Scom/scom-alerts.sql
@@ -5,7 +5,7 @@ SELECT
   CONVERT([varchar](64), COALESCE(rules.RuleId, monitor.MonitorId)) AS rule_monitor_id,
   alert.TicketId AS ticket_id,
   COALESCE(rules.RuleCategory, monitor.MonitorCategory) AS category,
- 
+
   CASE alert.Severity
     WHEN 2 THEN 'critical'
     WHEN 1 THEN 'warning'
@@ -19,19 +19,20 @@ SELECT
 
   alert.ResolutionState AS resolution_state,
   rs.ResolutionStateName AS resolution_state_name,
- 
+
   LOWER(topentity.Name) AS entity_name,
   topentitybasetype.TypeName AS entity_base_type,
   topentitytype.TypeName AS entity_type,
   entitytype.TypeName AS object_type,
- 
+
   COALESCE(prettyalert.AlertName, rules.RuleName, monitor.MonitorName, alert.AlertName) as alert_name,
-  prettyalert.AlertDescription as description
- 
+  prettyalert.AlertDescription as description,
+  MM.IsInMaintenanceMode
+
 FROM OperationsManager.dbo.Alert alert
 JOIN OperationsManager.dbo.BaseManagedEntity entity ON entity.BaseManagedEntityId = alert.BaseManagedEntityId
 JOIN OperationsManager.dbo.ResolutionState rs ON rs.ResolutionState = alert.ResolutionState
- 
+
 LEFT JOIN OperationsManagerDW.Alert.vAlert prettyalert ON prettyalert.AlertGuid = alert.AlertId
 LEFT JOIN OperationsManager.dbo.BaseManagedEntity topentity ON topentity.BaseManagedEntityId = entity.TopLevelHostEntityId
 LEFT JOIN OperationsManager.dbo.ManagedType entitytype ON entitytype.ManagedTypeId = entity.BaseManagedTypeId
@@ -39,7 +40,8 @@ LEFT JOIN OperationsManager.dbo.ManagedType topentitytype ON topentitytype.Manag
 LEFT JOIN OperationsManager.dbo.ManagedType topentitybasetype ON topentitybasetype.ManagedTypeId = topentitytype.BaseManagedTypeId
 LEFT JOIN OperationsManager.dbo.Rules rules ON rules.RuleId = alert.RuleId
 LEFT JOIN OperationsManager.dbo.Monitor monitor ON monitor.MonitorId = alert.RuleId
- 
+LEFT JOIN OperationsManager.dbo.MaintenanceMode mm ON mm.BaseManagedEntityId = alert.BaseManagedEntityId
+
 WHERE alert.ResolutionState = 0
-  AND alert.Severity >= 0
+  AND alert.Severity > 0
 ORDER BY alert.Severity DESC, alert.TimeRaised DESC
