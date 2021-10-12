2b88f2fed442e70986817bc6f9e79ccb25a125ff
Thomas Gelf <thomas@gelf.net>
2021-10-12 14:58:35 +0200

    DbFactory: use new factory classes

--- library/Eventtracker/DbFactory.php
+++ library/Eventtracker/DbFactory.php
@@ -2,13 +2,19 @@
 
 namespace Icinga\Module\Eventtracker;
 
+use gipfl\ZfDb\Db;
 use Icinga\Application\Config;
 use Icinga\Data\Db\DbConnection;
+use Icinga\Module\Eventtracker\Config\IcingaResource;
+use Icinga\Module\Eventtracker\Db\ZfDbConnectionFactory;
 
 class DbFactory
 {
     /** @var \Zend_Db_Adapter_Pdo_Abstract */
     protected static $db;
+
+    /** @var Db */
+    protected static $gipflDb;
 
     /**
      * @return \Zend_Db_Adapter_Pdo_Abstract
@@ -22,5 +28,20 @@ class DbFactory
         }
 
         return self::$db;
+    }
+
+    /**
+     * @return \gipfl\ZfDb\Adapter\Adapter
+     * @throws \Icinga\Exception\ConfigurationError
+     */
+    public static function gipflDb()
+    {
+        if (self::$gipflDb === null) {
+            self::$gipflDb = ZfDbConnectionFactory::connection(
+                IcingaResource::requireResourceConfig(Config::module('eventtracker')->get('db', 'resource'), 'db')
+            );
+        }
+
+        return self::$gipflDb;
     }
 }
