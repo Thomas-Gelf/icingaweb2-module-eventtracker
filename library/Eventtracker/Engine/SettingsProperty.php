d7b90d852d92af5d7315ff0505088c8b386d449a
Thomas Gelf <thomas@gelf.net>
2021-10-12 17:50:54 +0200

    SettingsProperty: simple trait

---
+++ library/Eventtracker/Engine/SettingsProperty.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Icinga\Module\Eventtracker\Engine;
+
+use Icinga\Module\Eventtracker\Modifier\Settings;
+
+trait SettingsProperty
+{
+    protected $settings;
+
+    protected function setSettings(Settings $settings)
+    {
+        $this->settings = $settings;
+    }
+
+    public function getSettings()
+    {
+        return $this->settings;
+    }
+}
