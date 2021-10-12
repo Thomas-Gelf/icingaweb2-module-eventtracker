f5a6d20e3f096be527a7f296545837e9cbb7c00b
Thomas Gelf <thomas@gelf.net>
2021-10-12 15:01:37 +0200

    ModifierUtils: describe a configured modifier

---
+++ library/Eventtracker/Modifier/ModifierUtils.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Icinga\Module\Eventtracker\Modifier;
+
+use Icinga\Module\Eventtracker\Data\PlainObjectRenderer;
+
+class ModifierUtils
+{
+    public static function describeModifier($propertyName, Modifier $modifier)
+    {
+        $info = $modifier::getName() . "($propertyName";
+        $settings = $modifier->getSettings()->jsonSerialize();
+        if (! empty((array) $settings)) {
+            $info .= ', ' . PlainObjectRenderer::render($settings);
+        }
+
+        return "$info)";
+    }
+}
