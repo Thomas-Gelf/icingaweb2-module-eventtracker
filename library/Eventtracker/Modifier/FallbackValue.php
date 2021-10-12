141f6fb2ff397a5b3244ad12b3c027278db81c1e
Thomas Gelf <thomas@gelf.net>
2021-10-12 15:17:05 +0200

    FallbackValue: new property modifier

---
+++ library/Eventtracker/Modifier/FallbackValue.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Icinga\Module\Eventtracker\Modifier;
+
+class FallbackValue extends BaseModifier
+{
+    protected static $name = 'Set a fallback value';
+
+    public function transform($object, $propertyName)
+    {
+        $value = ObjectUtils::getSpecificValue($object, $propertyName);
+        if ($value === null) {
+            return $this->getSettings()->getRequired('value');
+        }
+
+        return $this->simpleTransform($value);
+    }
+}
