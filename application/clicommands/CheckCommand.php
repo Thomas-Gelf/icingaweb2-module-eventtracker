a0451396015597e995ddc778ff8a88b17bd039bc
Thomas Gelf <thomas@gelf.net>
2021-11-24 13:55:34 +0100

    CheckCommand: refactor SCOM check

--- application/clicommands/CheckCommand.php
+++ application/clicommands/CheckCommand.php
@@ -2,31 +2,72 @@
 
 namespace Icinga\Module\Eventtracker\Clicommands;
 
+use gipfl\ZfDb\Adapter\Pdo\Mssql;
+use Icinga\Module\Eventtracker\Check\PluginState;
 use Icinga\Module\Eventtracker\Scom\Scom;
 use Icinga\Module\Eventtracker\Scom\ScomQuery;
 
 class CheckCommand extends Command
 {
-    protected $worstState = 0;
-
     /**
      * Run checks against the SCOM MSSQL DB
      */
     public function scomAction()
     {
-        $resource = $this->Config()->get('scom', 'db_resource');
-        if ($resource === null) {
-            $resource = $this->params->get('db-resource');
+        try {
+            $issues = $this->fetchScomIssues(
+                $this->requireMssqlResource($this->requireDbResourceName()),
+                $this->requireParam('host'),
+                $this->params->get('service')
+            );
+        } catch (\Exception $e) {
+            $state = PluginState::unknown();
+            $this->showWithState($e->getMessage(), $state);
+            exit($state->getExitCode());
         }
-        if ($resource === null) {
-            $this->fail('Got neither a configured [scom] db_resource nor --db-resource');
+        if (empty($issues)) {
+            $state = PluginState::ok();
+            $this->showWithState('No related SCOM alert has been found', $state);
+            exit($state->getExitCode());
         }
-        $host = $this->params->get('host');
-        if ($host === null) {
-            $this->fail('The --host parameter is required');
+
+        $criticals = [];
+        $warnings = [];
+        $state = new PluginState();
+        foreach ($issues as $issue) {
+            if ($issue->resolution_state === Scom::RESOLUTION_STATE_RESOLVED) {
+                continue;
+            }
+            if ($issue->in_maintenance) {
+                $state->raise(PluginState::STATE_WARNING);
+                $warnings[] = $issue;
+            } else {
+                if ($issue->alert_severity === 'critical') {
+                    $state->raise(PluginState::STATE_CRITICAL);
+                    $criticals[] = $issue;
+                } elseif ($issue->alert_severity === 'warning') {
+                    $state->raise(PluginState::STATE_WARNING);
+                    $warnings[] = $issue;
+                }
+            }
         }
-        $service = $this->params->get('service');
-        $db = $this->requireMssqlResource($resource);
+        $issues = array_merge($criticals, $warnings);
+        if (empty($issues)) {
+            $this->showWithState('SCOM has related alerts, but no current issues', PluginState::ok());
+        } elseif (\count($issues) === 1) {
+            $this->showWithState(\reset($issues)->description, $state);
+        } else {
+            $this->showWithState(
+                \reset($issues)->description
+                . \sprintf("\n...and %d more alerts", \count($issues) - 1),
+                $state
+            );
+        }
+        exit($state->getExitCode());
+    }
+
+    protected function fetchScomIssues(Mssql $db, $host, $service = null)
+    {
         $columns = ScomQuery::getDefaultColumns();
         $query = ScomQuery::prepareBaseQuery($db)
             ->columns($columns)
@@ -37,82 +78,52 @@ class CheckCommand extends Command
             $query->where($columns['alert_name'] . $db->quoteInto(' = ?', $service));
         }
 
-        try {
-            $issues = $db->fetchAll($query);
-        } catch (\Exception $e) {
-            $this->showWithState($e->getMessage(), 3);
-            exit(3);
-        }
-        if (empty($issues)) {
-            $this->showWithState('No related SCOM alert has been found');
-            exit(0);
-        }
-
-        $criticals = [];
-        $warnings = [];
-        foreach ($issues as $issue) {
-            if ($issue->resolution_state === Scom::RESOLUTION_STATE_RESOLVED) {
-                continue;
-            }
-            if ($issue->in_maintenance) {
-                $this->raiseWorstState(1);
-                $warnings[] = $issue;
-            } else {
-                if ($issue->alert_severity === 'critical') {
-                    $this->raiseWorstState(2);
-                    $criticals[] = $issue;
-                } elseif ($issue->alert_severity === 'warning') {
-                    $this->raiseWorstState(1);
-                    $warnings[] = $issue;
-                }
-            }
-        }
-        $issues = array_merge($criticals, $warnings);
-        if (empty($issues)) {
-            $this->showWithState('SCOM has related alerts, but no current issues');
-        } elseif (\count($issues) === 1) {
-            $this->showWithState(\reset($issues)->description);
-        } else {
-            $this->showWithState(
-                \reset($issues)->description
-                . \sprintf("\n...and %d more alerts", \count($issues) - 1)
-            );
-        }
-        exit($this->worstState);
+        return $db->fetchAll($query);
     }
 
-    protected function showWithState($message, $state = null)
+    protected function requireDbResourceName()
     {
-        \printf("[%s] %s\n", $this->getStateName($state), \rtrim($message, "\n"));
+        $resource = $this->Config()->get('scom', 'db_resource');
+        if ($resource === null) {
+            $resource = $this->params->get('db-resource');
+        }
+        if ($resource === null) {
+            $this->fail('Got neither a configured [scom] db_resource nor --db-resource');
+        }
+
+        return $resource;
     }
 
-    protected function getStateName($state = null)
+    protected function showWithState($message, PluginState $state)
+    {
+        \printf("[%s] %s\n", $state, \rtrim($message, "\n"));
+    }
+
+    /**
+     * @param $name
+     * @return mixed
+     */
+    protected function requireParam($name)
+    {
+        $value = $this->params->get('host');
+        if ($value === null) {
+            $this->fail("The --$name parameter is required");
+        }
+
+        return $value;
+    }
+
+    /**
+     * @param $msg
+     * @param PluginState|null $state
+     * @return never-returns
+     */
+    public function fail($msg, PluginState $state = null)
     {
         if ($state === null) {
-            $state = $this->worstState;
+            $state = PluginState::unknown();
         }
-
-        $states = [
-            'OK',
-            'WARNING',
-            'CRITICAL',
-            'UNKNOWN'
-        ];
-
-        return $this->screen->colorize($states[$state]);
-    }
-
-    protected function raiseWorstState($state)
-    {
-        if ($state > $this->worstState) {
-            $this->worstState = $state;
-            // TODO: Unknown VS Critical - however, there is no Unknown right now
-        }
-    }
-
-    public function fail($message)
-    {
-        $this->showWithState($message, 3);
-        exit(3);
+        $this->showWithState($msg, PluginState::unknown());
+        exit($state->getExitCode());
     }
 }
