11a5142334808ce3174c84ef3a9863c07bfe918b
Eric Lippmann <eric.lippmann@icinga.com>
2022-05-10 14:52:23 +0200

    Schema: Support files

--- schema/mysql.sql
+++ schema/mysql.sql
@@ -163,6 +163,32 @@ CREATE TABLE issue_activity (
 
 INSERT INTO sender (id, sender_name, implementation) VALUES (99999, 'compat', 'Compat');
 
+CREATE TABLE file(
+  checksum binary(20) NOT NULL COMMENT 'sha1(data)',
+  data mediumblob NOT NULL COMMENT 'maximum length of 16777215 (2^24 - 1) bytes, or 16MB in storage',
+  size mediumint unsigned NOT NULL COMMENT 'max value 16777215',
+  mime_type varchar(255) NOT NULL,
+  ctime bigint(20) NOT NULL,
+  PRIMARY KEY (checksum)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_bin;
+
+CREATE TABLE issue_file (
+  issue_uuid VARBINARY(16) NOT NULL,
+  file_checksum binary(20) NOT NULL,
+  filename varchar(255) NOT NULL,
+  ctime bigint(20) NOT NULL,
+  PRIMARY KEY (issue_uuid, file_checksum),
+  CONSTRAINT fk_issue_file_issue
+    FOREIGN KEY (issue_uuid)
+      REFERENCES issue (issue_uuid)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE,
+  CONSTRAINT fk_issue_file_file
+    FOREIGN KEY (file_checksum)
+      REFERENCES file (checksum)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_bin;
 
 CREATE TABLE issue_history (
   issue_uuid VARBINARY(16) NOT NULL,
@@ -240,7 +266,6 @@ CREATE TABLE eventtracker_schema_migration (
   PRIMARY KEY(schema_version)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_bin;
 
-
 INSERT INTO eventtracker_schema_migration
   (schema_version, migration_time)
-VALUES (4, NOW());
+VALUES (5, NOW());
