cc777b6dc0f0d0d2a1cc2a440cb5c43c10b67515
Eric Lippmann <eric.lippmann@icinga.com>
2022-08-30 15:50:55 +0200

    Action: Add enabled and filter

--- library/Eventtracker/Engine/Action.php
+++ library/Eventtracker/Engine/Action.php
@@ -2,6 +2,18 @@
 
 namespace Icinga\Module\Eventtracker\Engine;
 
+use Icinga\Data\Filter\Filter;
+use Icinga\Module\Eventtracker\Issue;
+
 interface Action extends Task
 {
+    public function setEnabled(bool $enabled);
+
+    public function isEnabled(): bool;
+
+    public function setFilter($filter);
+
+    public function getFilter(): ?Filter;
+
+    public function process(Issue $issue): void;
 }
