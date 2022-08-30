16210a53d53fe18323569b3551c4ffa7420ad975
Eric Lippmann <eric.lippmann@icinga.com>
2022-08-30 10:26:53 +0200

    Introduce Task interface as base for Action and Input

--- library/Eventtracker/Engine/Action.php
+++ library/Eventtracker/Engine/Action.php
@@ -2,31 +2,6 @@
 
 namespace Icinga\Module\Eventtracker\Engine;
 
-use Icinga\Module\Eventtracker\Modifier\Settings;
-use Psr\Log\LoggerAwareInterface;
-use Ramsey\Uuid\UuidInterface;
-use React\EventLoop\LoopInterface;
-
-interface Action extends LoggerAwareInterface
+interface Action extends Task
 {
-    public function __construct(Settings $settings, UuidInterface $uuid, $name);
-
-    public function getUuid(): UuidInterface;
-
-    public function getName();
-
-    public function getSettings(): Settings;
-
-    public function run(LoopInterface $loop);
-
-    public function start();
-    public function stop();
-    public function pause();
-    public function resume();
-
-    public static function getFormExtension(): FormExtension;
-
-    public static function getLabel();
-
-    public static function getDescription();
 }
--- library/Eventtracker/Engine/Input.php
+++ library/Eventtracker/Engine/Input.php
@@ -2,33 +2,6 @@
 
 namespace Icinga\Module\Eventtracker\Engine;
 
-use Evenement\EventEmitterInterface;
-use Icinga\Module\Eventtracker\Modifier\Settings;
-use Icinga\Module\Eventtracker\Web\Form;
-use Psr\Log\LoggerAwareInterface;
-use Ramsey\Uuid\UuidInterface;
-use React\EventLoop\LoopInterface;
-
-interface Input extends EventEmitterInterface, LoggerAwareInterface
+interface Input extends Task
 {
-    public function __construct(Settings $settings, UuidInterface $uuid, $name);
-
-    public function getUuid(): UuidInterface;
-
-    public function getName();
-
-    public function getSettings(): Settings;
-
-    public function run(LoopInterface $loop);
-
-    public function start();
-    public function stop();
-    public function pause();
-    public function resume();
-
-    public static function getFormExtension(): FormExtension;
-
-    public static function getLabel();
-
-    public static function getDescription();
 }
