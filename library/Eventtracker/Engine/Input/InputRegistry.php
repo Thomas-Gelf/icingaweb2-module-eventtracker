b2717937bc2e5aad14e8c8bd125f0b124aebb7c3
Eric Lippmann <eric.lippmann@icinga.com>
2022-08-30 13:41:59 +0200

    InputRegistry: Extend SimpleRegistry

--- library/Eventtracker/Engine/Input/InputRegistry.php
+++ library/Eventtracker/Engine/Input/InputRegistry.php
@@ -2,43 +2,14 @@
 
 namespace Icinga\Module\Eventtracker\Engine\Input;
 
-use Icinga\Module\Eventtracker\Engine\Input;
-use Icinga\Module\Eventtracker\Engine\Registry;
+use Icinga\Module\Eventtracker\Engine\SimpleRegistry;
 
-class InputRegistry implements Registry
+class InputRegistry extends SimpleRegistry
 {
     protected $implementations = [
         'syslog'     => SyslogInput::class,
         'kafka'      => KafkaInput::class,
         'restApi'    => RestApiInput::class,
-        // 'scom_alert' =>
+//        'scom_alert' => ...
     ];
-
-    public function getInstance(string $identifier): Input
-    {
-        $class = $this->getClassName($identifier);
-        return new $class;
-    }
-
-    public function addImplementation(string $identifier, string $class)
-    {
-        $this->implementations[$identifier] = $class;
-    }
-
-    public function getClassName(string $identifier): string
-    {
-        // TODO: Throw if not set
-        return $this->implementations[$identifier];
-    }
-
-    public function listImplementations(): array
-    {
-        /** @var string|Input $class */
-        $implementations = [];
-        foreach ($this->implementations as $key => $class) {
-            $implementations[$key] = $class::getLabel();
-        }
-
-        return $implementations;
-    }
 }
