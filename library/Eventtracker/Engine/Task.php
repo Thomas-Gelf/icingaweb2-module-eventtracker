05ee241cd8d6d12726c586b306ee467497949020
Eric Lippmann <eric.lippmann@icinga.com>
2022-08-30 10:26:53 +0200

    Introduce Task interface as base for Action and Input

---
+++ library/Eventtracker/Engine/Task.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Icinga\Module\Eventtracker\Engine;
+
+use Evenement\EventEmitterInterface;
+use Icinga\Module\Eventtracker\Modifier\Settings;
+use Psr\Log\LoggerAwareInterface;
+use Ramsey\Uuid\UuidInterface;
+use React\EventLoop\LoopInterface;
+
+interface Task extends EventEmitterInterface, LoggerAwareInterface
+{
+    public function __construct(Settings $settings, UuidInterface $uuid, $name);
+
+    public function getUuid(): UuidInterface;
+
+    public function getName();
+
+    public function getSettings(): Settings;
+
+    public function run(LoopInterface $loop);
+
+    public function start();
+    public function stop();
+    public function pause();
+    public function resume();
+
+    public static function getFormExtension(): FormExtension;
+
+    public static function getLabel();
+
+    public static function getDescription();
+}
