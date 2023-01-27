b253065b29f52bddf606ed12e8d370c8a01ae780
Thomas Gelf <thomas@gelf.net>
2023-01-27 10:36:02 +0100

    ConfigurationController: remove functionality...
    ...which hasn't been implemented in this branch

--- application/controllers/ConfigurationController.php
+++ application/controllers/ConfigurationController.php
@@ -4,36 +4,25 @@ namespace Icinga\Module\Eventtracker\Controllers;
 
 use gipfl\IcingaWeb2\Link;
 use gipfl\IcingaWeb2\Url;
-use gipfl\Json\JsonDecodeException;
 use gipfl\Json\JsonString;
 use gipfl\Web\Widget\Hint;
 use gipfl\ZfDbStore\DbStorableInterface;
 use gipfl\ZfDbStore\ZfDbStore;
 use Icinga\Module\Eventtracker\Db\ConfigStore;
 use Icinga\Module\Eventtracker\Engine\Action\ActionRegistry;
-use Icinga\Module\Eventtracker\Engine\Bucket\BucketRegistry;
 use Icinga\Module\Eventtracker\Engine\Downtime\DowntimeRule;
-use Icinga\Module\Eventtracker\Modifier\ModifierChain;
 use Icinga\Module\Eventtracker\Web\Form\ActionConfigForm;
 use Icinga\Module\Eventtracker\Web\Form\ApiTokenForm;
-use Icinga\Module\Eventtracker\Web\Form\BucketConfigForm;
 use Icinga\Module\Eventtracker\Web\Form\ChannelConfigForm;
 use Icinga\Module\Eventtracker\Web\Form\DowntimeForm;
-use Icinga\Module\Eventtracker\Web\Form\HostListForm;
 use Icinga\Module\Eventtracker\Web\Form\InputConfigForm;
 use Icinga\Module\Eventtracker\Engine\Input\InputRegistry;
 use Icinga\Module\Eventtracker\Web\Form\UuidObjectForm;
 use Icinga\Module\Eventtracker\Web\Table\ApiTokensTable;
 use Icinga\Module\Eventtracker\Web\Table\BaseTable;
-use Icinga\Module\Eventtracker\Web\Table\ChannelRulesTable;
-use Icinga\Module\Eventtracker\Web\Table\ConfiguredBucketsTable;
 use Icinga\Module\Eventtracker\Web\Table\ConfiguredChannelsTable;
-use Icinga\Module\Eventtracker\Web\Table\ConfiguredHostListsTable;
 use Icinga\Module\Eventtracker\Web\Table\ConfiguredInputsTable;
 use Icinga\Module\Eventtracker\Web\Table\ConfiguredActionsTable;
-use Icinga\Module\Eventtracker\Web\Table\DowntimeRulesTable;
-use Icinga\Module\Eventtracker\Web\Table\DowntimeScheduleTable;
-use ipl\Html\Html;
 use Ramsey\Uuid\Uuid;
 use Ramsey\Uuid\UuidInterface;
 
@@ -89,34 +78,6 @@ class ConfigurationController extends Controller
                 'form_class'  => ActionConfigForm::class,
                 'registry'    => ActionRegistry::class,
             ],
-            'buckets' => [
-                'singular' => $this->translate('Bucket'),
-                'plural'   => $this->translate('Buckets'),
-                'table'    => 'bucket',
-                'list_url' => 'eventtracker/configuration/buckets',
-                'url'      => 'eventtracker/configuration/bucket',
-                'table_class' => ConfiguredBucketsTable::class,
-                'form_class'  => BucketConfigForm::class,
-                'registry'    => BucketRegistry::class,
-            ],
-            'downtimes' => [
-                'singular' => $this->translate('Downtime'),
-                'plural'   => $this->translate('Downtimes'),
-                'table'    => 'downtime_rule',
-                'list_url' => 'eventtracker/configuration/downtimes',
-                'url'      => 'eventtracker/configuration/downtime',
-                'table_class' => DowntimeRulesTable::class,
-                'form_class'  => DowntimeForm::class,
-            ],
-            'hostlists' => [
-                'singular' => $this->translate('Host list'),
-                'plural'   => $this->translate('Host lists'),
-                'table'    => 'host_list',
-                'list_url' => 'eventtracker/configuration/hostlists',
-                'url'      => 'eventtracker/configuration/hostlist',
-                'table_class' => ConfiguredHostListsTable::class,
-                'form_class'  => HostListForm::class,
-            ],
         ];
     }
 
@@ -168,9 +129,6 @@ class ConfigurationController extends Controller
         $this->addObjectTab();
         $form = $this->getForm();
         $this->content()->add($form);
-        if ($rules = $form->getElementValue('rules')) {
-            $this->showRules($this->requireUuid(), $rules);
-        }
     }
 
     public function actionsAction()
@@ -184,67 +142,6 @@ class ConfigurationController extends Controller
         $this->variant = 'actions';
         $this->addObjectTab();
         $this->content()->add($this->getForm());
-    }
-
-    public function bucketsAction()
-    {
-        $this->variant = 'buckets';
-        $this->showList();
-    }
-
-    public function bucketAction()
-    {
-        $this->variant = 'buckets';
-        $this->addObjectTab();
-        $this->content()->add($this->getForm());
-    }
-
-    public function downtimesAction()
-    {
-        $this->variant = 'downtimes';
-        $this->showList();
-    }
-
-    public function downtimeAction()
-    {
-        $this->variant = 'downtimes';
-        $this->addObjectTab();
-        /** @var DowntimeForm $form */
-        $form = $this->getForm();
-        $this->content()->add($form);
-        if ($form->hasObject()) {
-            $this->content()->add(new DowntimeScheduleTable($this->db(), $form->getObject()));
-        }
-    }
-
-    public function hostlistsAction()
-    {
-        $this->variant = 'hostlists';
-        $this->showList();
-    }
-
-    public function hostlistAction()
-    {
-        $this->variant = 'hostlists';
-        $this->addObjectTab();
-        $this->content()->add($this->getForm());
-    }
-
-    protected function showRules(UuidInterface $uuid, $rules)
-    {
-        try {
-            $modifiers = ModifierChain::fromSerialization(JsonString::decode($rules));
-        } catch (JsonDecodeException $e) {
-            return;
-        }
-        $info = Html::tag('ul');
-        foreach ($modifiers->getModifiers() as list($propertyName, $modifier)) {
-            $info->add(Html::tag('li', ModifierUtils::describeModifier($propertyName, $modifier)));
-        }
-        $this->content()->add([
-            Html::tag('h3', $this->translate('Configured Rules')),
-            $info
-        ]);
     }
 
     protected function showList()
