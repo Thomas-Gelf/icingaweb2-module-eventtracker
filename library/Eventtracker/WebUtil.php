26d11514b32652b5afc6f51230b94eb22d6d60f8
Thomas Gelf <thomas@gelf.net>
2023-03-03 10:27:45 +0100

    WebUtil: some new web-related helper methods

---
+++ library/Eventtracker/WebUtil.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Icinga\Module\Eventtracker;
+
+use Icinga\Date\DateFormatter;
+use ipl\Html\Error as HtmlError;
+use ipl\Html\Html;
+use ipl\Html\HtmlDocument;
+use Throwable;
+use Exception;
+
+class WebUtil
+{
+    public static function runFailSafe($callback, HtmlDocument $parent)
+    {
+        try {
+            $callback();
+        } catch (Exception $e) {
+            $parent->add(HtmlError::show($e));
+        } catch (Throwable $e) {
+            $parent->add(HtmlError::show($e));
+        }
+    }
+
+    public static function timeAgo($time)
+    {
+        return Html::tag('span', [
+            'class' => 'time-ago',
+            'title' => DateFormatter::formatDateTime($time)
+        ], DateFormatter::timeAgo($time));
+    }
+}
