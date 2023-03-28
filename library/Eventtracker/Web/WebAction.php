2229adb0b01b2974ae3daba042065b21f39c7142
Thomas Gelf <thomas@gelf.net>
2023-03-28 13:30:13 +0200

    WebAction: type hints

--- library/Eventtracker/Web/WebAction.php
+++ library/Eventtracker/Web/WebAction.php
@@ -2,17 +2,33 @@
 
 namespace Icinga\Module\Eventtracker\Web;
 
+use Icinga\Module\Eventtracker\Engine\Registry;
+use Icinga\Module\Eventtracker\Web\Form\UuidObjectForm;
+use Icinga\Module\Eventtracker\Web\Table\BaseTable;
+
 class WebAction
 {
+    /** @var string */
+    public $name;
+    /** @var string */
     public $singular;
+    /** @var string */
     public $plural;
+    /** @var string */
     public $description;
+    /** @var string */
     public $table;
+    /** @var string */
     public $listUrl;
+    /** @var string */
     public $url;
+    /** @var string */
     public $icon;
+    /** @var class-string<BaseTable> */
     public $tableClass;
+    /** @var class-string<UuidObjectForm> */
     public $formClass;
+    /** @var ?class-string<Registry> */
     public $registry;
 
     public static function create(array $properties): WebAction
