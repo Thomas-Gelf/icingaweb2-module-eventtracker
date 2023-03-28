2beb212125847dbf53bc83bda33c4d58a1788314
Thomas Gelf <thomas@gelf.net>
2023-03-28 16:51:00 +0200

    js: add control-collapsible (rules)

--- public/js/module.js
+++ public/js/module.js
@@ -9,11 +9,21 @@
     Eventtracker.prototype = {
         initialize: function () {
             this.module.on('mouseover', 'thead tr', this.checkForHeaderHref);
+            this.module.on('click', 'td a.control-collapsible', this.toggleCollapsibleRow);
         },
 
         checkForHeaderHref: function (ev) {
             // href will be added because of sort icons
             $(ev.currentTarget).removeAttr('href');
+        },
+
+        toggleCollapsibleRow: function (ev) {
+            $(ev.currentTarget).closest('td.collapsible-table-row').toggleClass('collapsed');
+            $('#col1').removeData('icinga-actiontable-former-href');
+            ev.stopPropagation();
+            ev.stopImmediatePropagation();
+            ev.preventDefault();
+            return false;
         }
     };
 
