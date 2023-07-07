1ddb3bbd0a43820893d8da602c2d00e1bda9f7c1
Thomas Gelf <thomas@gelf.net>
2023-07-07 07:21:30 +0200

    ConfigHelperCi: move CI-related logic

--- library/Eventtracker/ConfigHelper.php
+++ library/Eventtracker/ConfigHelper.php
@@ -16,69 +16,10 @@ class ConfigHelper
         }
     }
 
+    // has been moved to ConfigHelperCi, remains here for iET Compat
     public static function fillPlaceHoldersForIssue($string, Issue $issue, ZfDb $db)
     {
-        $ci = IcingaCi::eventuallyLoadForIssue($db, $issue);
-        if ($ci) {
-            if ($ci->object_type === 'service') {
-                if ($host = IcingaCi::eventuallyLoad($db, $issue->get('host_name'))) {
-                    $ci->host = $host;
-                }
-            }
-        }
-        return \preg_replace_callback('/({[^}]+})/', function ($match) use ($issue, $ci) {
-            $property = \trim($match[1], '{}');
-            list($property, $modifier) = static::extractPropertyModifier($property);
-            if (\preg_match('/^(host|service)\.(.+)$/', $property, $match)) {
-                if ($ci === null) {
-                    return static::missingProperty($property);
-                }
-                $value = static::getIcingaCiProperty($ci, $property);
-            } else {
-                $value = static::getIssueProperty($issue, $property);
-            }
-
-            static::applyPropertyModifier($value, $modifier);
-
-            return $value;
-        }, $string);
-    }
-
-
-    /**
-     * @param \stdClass $ci
-     * @param $property
-     * @return null
-     */
-    protected static function getIcingaCiProperty($ci, $property)
-    {
-        if (\preg_match('/^(host|service)\.(.+)$/', $property, $match)) {
-            if ($ci->object_type === 'service') {
-                if (! isset($ci->host)) {
-                    return null;
-                }
-
-                $ci = $ci->host;
-            }
-            $property = $match[2];
-        }
-
-        return static::reallyGetCiProperty($ci, $property);
-    }
-
-    protected static function reallyGetCiProperty($ci, $property)
-    {
-        if (\preg_match('/^vars\.(.+)$/', $property, $match)) {
-            $varName = $match[1];
-            if (isset($ci->vars->$varName)) {
-                return $ci->vars->$varName;
-            }
-        }
-        if (isset($ci->$property)) {
-            return $ci->$property;
-        }
-
-        return null;
+        return ConfigHelperCi::fillPlaceHoldersForIssue($string, $issue, $db);
     }
 
     protected static function missingProperty($property)
