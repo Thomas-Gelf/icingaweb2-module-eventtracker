af18cb9822a2aecc782c2c59c7019c0be73934a0
Thomas Gelf <thomas@gelf.net>
2024-07-29 18:44:19 +0200

    SoapInteractiveActionForm: allow to trigger SOAP
    ...actions on demand

---
+++ library/Eventtracker/Soap/SoapInteractiveActionForm.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Icinga\Module\Eventtracker\Soap;
+
+use gipfl\Translation\TranslationHelper;
+use gipfl\Web\Form;
+use Icinga\Module\Eventtracker\ConfigHelper;
+use Icinga\Module\Eventtracker\Engine\Action\SoapAction;
+use Icinga\Module\Monitoring\Object\MonitoredObject;
+use ipl\Orm\Model;
+
+class SoapInteractiveActionForm extends Form
+{
+    use TranslationHelper;
+
+    /** @var SoapAction */
+    protected $soapAction;
+    protected $object;
+
+    /**
+     * @param SoapAction $action
+     * @param Model|MonitoredObject$object
+     */
+    public function __construct(SoapAction $action, $object)
+    {
+        $this->soapAction = $action;
+        $this->object = $object;
+    }
+
+    protected function assemble()
+    {
+        $action = $this->soapAction;
+        $settings = $action->getSettings();
+        $client = $action->getSoapClient();
+        $parser = SoapClientDefinitionParser::discover($client);
+        $methodParams = $parser->getFlatMethodProperties($action->getMethodName());
+        foreach ($methodParams as $name => $type) {
+            [$l, $r] = explode('.', $name, 2);
+            // Hint: There is something wrong with value handling in our form, dots didn't work
+            $paramName = $l . '/' . $r;
+            $value = $settings->get($paramName);
+
+            if ($value) {
+                $value = ConfigHelper::fillPlaceholders($value, $this->object, null, true);
+            }
+            $this->addElement('textarea', $paramName, [
+                'label' => "$name",
+                'rows'  => 1,
+                'value' => $value,
+            ]);
+        }
+        $this->addElement('submit', 'submit', [
+            'label' => $this->translate('Submit')
+        ]);
+    }
+}
