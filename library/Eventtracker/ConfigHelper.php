f897f9765dab181e91b6273ffaab062d659d9365
Thomas Gelf <thomas@gelf.net>
2024-07-29 19:29:09 +0200

    ConfigHelper: improve IcingaDb/MonitoredObject...
    ...support in a compatible way

--- library/Eventtracker/ConfigHelper.php
+++ library/Eventtracker/ConfigHelper.php
@@ -3,7 +3,11 @@
 namespace Icinga\Module\Eventtracker;
 
 use gipfl\ZfDb\Adapter\Adapter as ZfDb;
+use Icinga\Module\Icingadb\Model\CustomvarFlat;
+use Icinga\Module\Icingadb\Model\Host as IcingaDbHost;
+use Icinga\Module\Monitoring\Object\Host;
 use Icinga\Module\Monitoring\Object\MonitoredObject;
+use ipl\Orm\Model;
 
 class ConfigHelper
 {
@@ -22,8 +26,12 @@ class ConfigHelper
         return ConfigHelperCi::fillPlaceHoldersForIssue($string, $issue, $db);
     }
 
-    public static function missingProperty($property)
+    public static function missingProperty($property, bool $missingIsNull = false)
     {
+        if ($missingIsNull) {
+            return null;
+        }
+
         return '{' . $property . '}';
     }
 
@@ -50,15 +58,49 @@ class ConfigHelper
         return static::missingProperty($property);
     }
 
+    protected static function icingaHostStateMap($numeric)
+    {
+        switch ((int) $numeric) {
+            case 0:
+                return 'UP';
+            case 1:
+                return 'DOWN';
+            case 2:
+                return 'UNREACHABLE';
+            case 99:
+                return 'PENDING';
+            default:
+                return $numeric;
+        }
+    }
+
+    protected static function icingaServiceStateMap($numeric)
+    {
+        switch ((int) $numeric) {
+            case 0:
+                return 'OK';
+            case 1:
+                return 'WARNING';
+            case 2:
+                return 'CRITICAL';
+            case 3:
+                return 'UNKNOWN';
+            case 99:
+                return 'PENDING';
+            default:
+                return $numeric;
+        }
+    }
+
     /**
      * @param $string
      * @param Event|Issue|object $issue
      * @param callable|null $callback
      * @return string|null
      */
-    public static function fillPlaceholders($string, $issue, callable $callback = null)
+    public static function fillPlaceholders($string, $issue, callable $callback = null, bool $missingIsNull = false)
     {
-        $replace = function ($match) use ($issue) {
+        $replace = function ($match) use ($issue, $missingIsNull) {
             $property = \trim($match[1], '{}');
             list($property, $modifier) = static::extractPropertyModifier($property);
             if ($issue instanceof Issue) {
@@ -69,15 +111,101 @@ class ConfigHelper
             } elseif ($issue instanceof MonitoredObject) {
                 if (preg_match('/^(host|service)\.vars\.([^.]+)$/', $property, $pMatch)) {
                     $value = $issue->{'_' . $pMatch[1] . '_' . $pMatch[2]};
+                } elseif (preg_match('/^vars\.([^.]+)$/', $property, $pMatch)) {
+                    if ($issue instanceof Host) {
+                        $value = $issue->{'_host_' . $pMatch[1]};
+                    } else {
+                        $value = $issue->{'_service_' . $pMatch[1]};
+                        if ($value === null) {
+                            $value = $issue->{'_host_' . $pMatch[1]};
+                        }
+                    }
                 } else {
-                    $value = $issue->$property;
+                    if ($property === 'state') {
+                        if ($issue instanceof Host) {
+                            $value = self::icingaHostStateMap($issue->state);
+                        } else {
+                            $value = self::icingaServiceStateMap($issue->state);
+                        }
+                    } else {
+                        try {
+                            $value = $issue->$property;
+                        } catch (\Exception $e) {
+                            $value = null;
+                        }
+                    }
+                }
+            } elseif ($issue instanceof Model) {
+                if ($property === 'state') {
+                    $value = strtoupper($issue->state->getStateText());
+                } elseif ($property === 'host_name') {
+                    if ($issue instanceof IcingaDbHost) {
+                        $value = $issue->name;
+                    } else {
+                        $value = $issue->host->name;
+                    }
+                } elseif (in_array($property, ['service_name', 'service_description'])) {
+                    if ($issue instanceof IcingaDbHost) {
+                        $value = null;
+                    } else {
+                        $value = $issue->name;
+                    }
+                } elseif (in_array($property, ['output', 'long_output'])) {
+                    $value = $issue->state->$property;
+                } elseif (preg_match('/^(host|service)\.vars\.([^.]+)$/', $property, $pMatch)) {
+                    if ($issue instanceof IcingaDbHost) {
+                        if ($pMatch[1] === 'service') {
+                            $value = null;
+                        } else {
+                            $vars = (new CustomvarFlat())->unFlattenVars($issue->customvar_flat);
+                            $value = $vars[$pMatch[2]] ?? null;
+                        }
+                    } else {
+                        if ($pMatch[1] === 'service') {
+                            $vars = (new CustomvarFlat())->unFlattenVars($issue->customvar_flat);
+                            $value = $vars[$pMatch[2]] ?? null;
+                        } else {
+                            $vars = (new CustomvarFlat())->unFlattenVars($issue->host->customvar_flat);
+                            $value = $vars[$pMatch[2]] ?? null;
+                        }
+                    }
+                } elseif (preg_match('/^vars\.([^.]+)$/', $property, $pMatch)) {
+                    if ($issue instanceof IcingaDbHost) {
+                        $vars = (new CustomvarFlat())->unFlattenVars($issue->customvar_flat);
+                        $value = $vars[$pMatch[1]] ?? null;
+                    } else {
+                        $vars = (new CustomvarFlat())->unFlattenVars($issue->customvar_flat);
+                        $value = $vars[$pMatch[1]] ?? null;
+                        if ($value === null) {
+                            $vars = (new CustomvarFlat())->unFlattenVars($issue->host->customvar_flat);
+                            $value = $vars[$pMatch[1]] ?? null;
+                        }
+                    }
+                } else {
+                    try {
+                        $value = $issue->$property ?? null;
+                    } catch (\Exception $e) {
+                        $value = null;
+                    }
                 }
             } else {
-                $value = $issue->$property ?? null;
+                try {
+                    $value = $issue->$property ?? null;
+                } catch (\Exception $e) {
+                    $value = null;
+                }
             }
             if ($value === null) {
-                return static::missingProperty($property);
+                return static::missingProperty($property, $missingIsNull);
             }
+            if (! is_string($value)) {
+                throw new \RuntimeException(sprintf(
+                    'String value expected, got %s for %s',
+                    $property,
+                    get_debug_type($value)
+                ));
+            }
+
             static::applyPropertyModifier($value, $modifier);
 
             return $value;
