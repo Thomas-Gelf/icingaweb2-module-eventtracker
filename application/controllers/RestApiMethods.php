1b02d129e51e11cc54e8068f9b3b76bbac116c97
Thomas Gelf <thomas@gelf.net>
2024-11-14 16:51:47 +0100

    RestApiMethods: a bunch of methods for fetch

--- application/controllers/RestApiMethods.php
+++ application/controllers/RestApiMethods.php
@@ -3,10 +3,17 @@
 namespace Icinga\Module\Eventtracker\Controllers;
 
 use gipfl\IcingaWeb2\CompatController;
+use gipfl\IcingaWeb2\Url;
+use gipfl\IcingaWeb2\Zf1\Db\FilterRenderer;
 use gipfl\Json\JsonEncodeException;
 use gipfl\Json\JsonString;
 use gipfl\Web\Widget\Hint;
+use gipfl\ZfDb\Select;
+use Icinga\Data\Filter\Filter;
+use Icinga\Data\Filter\FilterChain;
+use Icinga\Data\Filter\FilterExpression;
 use Icinga\Exception\NotFoundError;
+use InvalidArgumentException;
 use Ramsey\Uuid\Uuid;
 use Ramsey\Uuid\UuidInterface;
 
@@ -114,5 +121,61 @@ trait RestApiMethods
         }
 
         return JsonString::decode($permissions);
+    }
+
+    protected static function applyColumnAndFilterParams(Select $query, Url $url, array $validColumns)
+    {
+        $columns = self::getColumnsFromUrlParam($url->getParams()->shift('columns'), $validColumns);
+        $query->columns($columns ?? '*');
+        $filter = Filter::fromQueryString($url->getQueryString());
+        foreach ($filter->listFilteredColumns() as $column) {
+            self::assertValidColumnName($column, $validColumns);
+        }
+        self::tweakFilterValues($filter);
+        FilterRenderer::applyToQuery($filter, $query);
+    }
+
+    protected static function tweakFilterValues(Filter $filter)
+    {
+        if ($filter instanceof FilterExpression) {
+            if (preg_match('/_uuid$/', $filter->getColumn())) {
+                $filter->setExpression(Uuid::fromString($filter->getExpression())->getBytes());
+            }
+        } elseif ($filter instanceof FilterChain) {
+            foreach ($filter->filters() as $subFilter) {
+                self::tweakFilterValues($subFilter);
+            }
+        }
+    }
+
+    protected static function getColumnsFromUrlParam(?string $param, ?array $validColumns): ?array
+    {
+        if ($param === null) {
+            return null;
+        }
+
+        $columns = preg_split('/\s*,\s*/', $param);
+        if ($columns) {
+            foreach ($columns as $column) {
+                self::assertValidColumnName($column, $validColumns);
+            }
+        } else {
+            return null;
+        }
+
+        return $columns;
+    }
+
+    protected static function assertValidColumnName(string $column, ?array $validColumns)
+    {
+        if ($validColumns) {
+            if (!in_array($column, $validColumns)) {
+                throw new InvalidArgumentException("'$column is not a valid column name");
+            }
+        }
+
+        if (! preg_match('/^[a-z][a-z0-9_]*[a-z0-9]/', $column)) {
+            throw new InvalidArgumentException("'$column is not a valid column name");
+        }
     }
 }
