99a80ef1d4742c6c2133fc636d4050c059d79995
Christoph Breit <christoph.breit@netways.de>
2025-02-05 17:14:04 +0100

    ConfigHistory: improve styling

--- library/Eventtracker/Web/Table/ConfigurationHistoryTable.php
+++ library/Eventtracker/Web/Table/ConfigurationHistoryTable.php
@@ -2,9 +2,14 @@
 
 namespace Icinga\Module\Eventtracker\Web\Table;
 
+use gipfl\Format\LocalDateFormat;
+use gipfl\Format\LocalTimeFormat;
 use gipfl\IcingaWeb2\Link;
+use gipfl\IcingaWeb2\Url;
 use gipfl\Translation\TranslationHelper;
-use Icinga\Module\Eventtracker\Time;
+use Icinga\Module\Eventtracker\Web\WebActions;
+use Icinga\Module\Eventtracker\Web\Widget\ConfigHistoryDetails;
+use ipl\Html\HtmlElement;
 
 class ConfigurationHistoryTable extends BaseTable
 {
@@ -14,31 +19,83 @@ class ConfigurationHistoryTable extends BaseTable
         'ch.action',
     ];
 
+    public function __construct($db, Url $url = null)
+    {
+        $this->actions = new WebActions();
+        parent::__construct($db, $url);
+    }
+    protected function renderTitleColumns()
+    {
+        return null;
+    }
+
+    public function renderRow($row): HtmlElement
+    {
+        if (isset($row->ts_modification)) {
+            $this->renderDayIfNew($this->formatTsHeader($row->ts_modification), count($this->getChosenColumns()));
+        }
+        return parent::renderRow($row);
+    }
+
+    public function renderDayIfNew($timestamp, $colspan = 2)
+    {
+        if ($this->lastDay !== $timestamp) {
+            $this->nextHeader()->add(
+                $this::th($timestamp, [
+                    'colspan' => $colspan,
+                    'class' => 'table-header-day'
+                ])
+            );
+
+            $this->lastDay = $timestamp;
+            $this->nextBody();
+        }
+    }
+
     protected function initialize()
     {
+
         $this->addAvailableColumns([
             $this->createColumn('ts_modification', $this->translate('Time'), [
                 'ts_modification' => 'ch.ts_modification',
                 'object_uuid' => 'ch.object_uuid',
             ])->setRenderer(function ($row) {
+//                return Html::tag('td', ['class' => $this->getRowClasses($row)], date('H:i', $row->ts_modification));
                 return $this->linkToModification($row->ts_modification);
             })->setDefaultSortDirection('DESC'),
+
             $this->createColumn('action', $this->translate('Action'), [
                 'action'      => 'ch.action',
+                'author'       => 'ch.author',
                 'label'       => 'ch.label',
                 'object_type' => 'ch.object_type',
             ])->setRenderer(function ($row) {
-                return sprintf('%s %s "%s"', $row->action, $row->object_type, $row->label);
+                return new ConfigHistoryDetails($this->actions, $row);
             }),
-            $this->createColumn('author', $this->translate('Author')),
         ]);
     }
 
     protected function linkToModification($ts)
     {
-        return Link::create(Time::agoFormatted($ts), 'eventtracker/history/configuration-change', [
+        $l = new LocalTimeFormat();
+        return Link::create($l->getShortTime($ts), 'eventtracker/history/configuration-change', [
             'ts' => $ts
         ]);
+    }
+
+    protected function formatTsHeader($ts)
+    {
+        $timestamp = (int) ($ts / 1000);
+        $now = time();
+        $formatter = new LocalDateFormat();
+        if ($timestamp > ($now - 120)) {
+            return $this->translate('Now');
+        }
+        if ($timestamp > ($now - 600)) {
+            return sprintf($this->translate('%d minutes ago'), floor(($now - $timestamp) / 60));
+        }
+
+        return $formatter->getFullDay($timestamp);
     }
 
     public function prepareQuery()
---
+++ library/Eventtracker/Web/Widget/ConfigHistoryDetails.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Icinga\Module\Eventtracker\Web\Widget;
+
+use gipfl\Translation\TranslationHelper;
+use Icinga\Module\Eventtracker\Web\WebActions;
+use ipl\Html\HtmlDocument;
+use RuntimeException;
+
+class ConfigHistoryDetails extends HtmlDocument
+{
+    use TranslationHelper;
+    public function __construct(WebActions $actions, $change)
+    {
+
+        $webAction = $actions->getByTableName($change->object_type);
+        $singular = $webAction->singular;
+        switch ($change->action) {
+            case 'create':
+                $actionName = sprintf(
+                    $this->translate('%s %s has been created by %s'),
+                    $singular,
+                    $change->label,
+                    $change->author
+                );
+                break;
+            case 'modify':
+                $actionName = sprintf(
+                    $this->translate('%s %s has been modified by %s'),
+                    $singular,
+                    $change->label,
+                    $change->author
+                );
+                break;
+            case 'delete':
+                $actionName = sprintf(
+                    $this->translate('%s %s has been deleted by %s'),
+                    $singular,
+                    $change->label,
+                    $change->author
+                );
+                break;
+            default:
+                throw new RuntimeException(sprintf('Invalid configuration change action: %s', $change->action));
+        }
+
+        $this->add($actionName);
+
+    }
+}
