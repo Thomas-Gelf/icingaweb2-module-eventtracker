9ff4592bb0df4f7f49d1263df85b5403a19af2ea
Thomas Gelf <thomas@gelf.net>
2025-05-19 17:34:04 +0200

    TimeSlot: implement serialization

--- library/Eventtracker/Engine/TimePeriod/TimeSlot.php
+++ library/Eventtracker/Engine/TimePeriod/TimeSlot.php
@@ -2,33 +2,51 @@
 
 namespace Icinga\Module\Eventtracker\Engine\TimePeriod;
 
-use DateTimeInterface;
+use DateTimeImmutable as DTI;
+use DateTimeInterface as DT;
+use gipfl\Json\JsonSerialization;
 
-class TimeSlot
+class TimeSlot implements JsonSerialization
 {
     /** @readonly  */
-    public ?DateTimeInterface $start;
+    public ?DT $start;
     /** @readonly  */
-    public ?DateTimeInterface $end;
+    public ?DT $end;
 
-    public function __construct(?DateTimeInterface $start, ?DateTimeInterface $end)
+    public function __construct(?DT $start, ?DT $end)
     {
         $this->start = $start ? clone $start : null;
         $this->end = $end ? clone $end : null;
     }
 
-    public function isActive(DateTimeInterface $now): bool
+    public function isActive(DT $now): bool
     {
         return ($this->start === null || $this->start <= $now)
             && ($this->end === null || $this->end >= $now);
     }
 
-    public function willBeActiveAfter(DateTimeInterface $now): bool
+    public function willBeActiveAfter(DT $now): bool
     {
         if ($this->end && $this->end < $now) {
             return false;
         }
 
         return true;
+    }
+
+    public function jsonSerialize():\stdClass
+    {
+        return (object) [
+            'start' => $this->start ? $this->start->format(DT::RFC3339_EXTENDED) : null,
+            'end' => $this->end ? $this->end->format(DT::RFC3339_EXTENDED) : null,
+        ];
+    }
+
+    public static function fromSerialization($any)
+    {
+        return new TimeSlot(
+            isset($any->start) ? DTI::createFromFormat(DT::RFC3339_EXTENDED, $any->start) : null,
+            isset($any->start) ? DTI::createFromFormat(DT::RFC3339_EXTENDED, $any->start) : null,
+        );
     }
 }
