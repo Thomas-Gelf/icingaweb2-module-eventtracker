2fdf6301ce6fe73926942f42889846a4c45e4f09
Christoph Breit <christoph.breit@netways.de>
2025-09-24 16:36:53 +0200

    add simulation of rules and form fields for the modifiers

---
+++ library/Eventtracker/Web/Form/SimulateRuleForm.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Icinga\Module\Eventtracker\Web\Form;
+
+use gipfl\Web\Form;
+use gipfl\Translation\TranslationHelper;
+use Icinga\Module\Eventtracker\SampleObject;
+
+class SimulateRuleForm extends Form
+{
+    use TranslationHelper;
+    protected $ns;
+    protected $sessionKey;
+
+    public function __construct($ns, $sessionKey)
+    {
+        $this->ns = $ns;
+        $this->sessionKey = $sessionKey;
+    }
+
+    protected function assemble()
+    {
+
+        $this->addElement('select', 'simulation_entries', [
+           'label' => $this->translate('Simulation entries'),
+           'description' => $this->translate('Simulation entries'),
+           'required' => true,
+            'options' => [null => $this->translate('- please choose -'), 'syslog' => $this->translate('Syslog')],
+            'autosubmit' => true,
+        ]);
+        if ($simulationEntry = $this->getValue('simulation_entries')) {
+            $sampleObject = SampleObject::getSampleObject($simulationEntry);
+            $this->ns->set($this->sessionKey, $sampleObject);
+        }
+        $this->addElement('submit', 'submit', [
+            'label' => $this->translate('Submit'),
+        ]);
+    }
+}
